31e95d91e73ab7071b8f5bdda8ca2e86944b69d2
Dali-Lopez <38229136+Dali-Lopez@users.noreply.github.com>
2021-12-13 23:34:30 -0600

    Hice cosas asi bien de jaker

--- appreact/src/componentes/pages/consultasbd/MostrarProducto.js
+++ appreact/src/componentes/pages/consultasbd/MostrarProducto.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react"
+import React, { Component } from 'react'; 
 import logosvg from '../../../img/logoNutricion.svg'
-// pruebas de fluid algo asi
+// Agregue las cards, ahora tengo que agregar imagenes, y listo yas estubo dijo el fontanero
 export default function MostrarProducto({dietas}){
     const [value, setValue] = useState()
     useEffect(()=>{
@@ -25,7 +26,9 @@ export default function MostrarProducto({dietas}){
                 <div key={cont} class="col-sm-4">
                     <div class="card">
                         <div class="card-body">
+                            
                             <h5 class="card-title">{valor.titulo}</h5>
+                                <img src={ require('./logoN.png') } width="100px" height="100px" alt=""></img>
                             <p class="card-text">{valor.descripcion}</p>
                             <a href="#" class="btn btn-primary">Saber mas</a>
                         </div>
